b461682075818f1005b16ab20f34a50fd24110f8
alan <alan.martnz@gmail.com>
2022-06-09 08:16:21 -0700

    commit tree ddc44a30256cea21127c0e489e28d7f3d5547d32

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/'.'&&'!'&&'?'/).count
   end
 end
+
+class String
+  def count_sentences
+    self.split(/[.!?]/).select{|value| value != ""}.count
+  end
+end
+# "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+# "hey this is crazy. But I just met you! Here's my number, so call me maybe?".count_sentences
